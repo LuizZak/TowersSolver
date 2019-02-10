f431fd6e68e1aea5ce2fd26200b951d4bf7f204b
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-02-10 21:11:32 -0200

    Correcting unit test class

--- Tests/CommonsTests/KeyTests.swift
+++ Tests/CommonsTests/KeyTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 import Commons
 
-class KeyTests: XCTest {
+class KeyTests: XCTestCase {
     func testIsEquatable() {
         XCTAssertEqual(Key<String, Int>(1), Key<String, Int>(1))
         XCTAssertNotEqual(Key<String, Int>(1), Key<String, Int>(2))
