9098e0a946a18a3af2326d127527496949911318
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-11-03 19:30:51 -0300

    [PatternSolver] Improving looks of console printed grid

--- Sources/PatternSolver/PatternGridPrinter.swift
+++ Sources/PatternSolver/PatternGridPrinter.swift
@@ -1,12 +1,194 @@
 import Console
 
 public class PatternGridPrinter: ConsolePrintBuffer {
+    private static let _darkTileChar: Character = "█"
+    private static let _lightTileChar: Character = " "
+    private static let _undecidedTileChar: Character = "?"
+
+    private var _edgePatterns: [GridEdgePattern] = []
+
     /// Initializes a grid printer with a buffer capable of rendering the given
     /// grid with each cell has 4 characters of width and 2 of height.
     public convenience init(bufferForGrid grid: PatternGrid) {
         self.init(
             bufferWidth: PatternGridPrinter.bufferWidth(for: grid),
             bufferHeight: PatternGridPrinter.bufferHeight(for: grid)
+        )
+
+        _initializeEdgePatterns()
+    }
+
+    private func _initializeEdgePatterns() {
+        typealias State = GridEdgePattern.StateMatch
+
+        func addRule(
+            state: State,
+            left: State = .any,
+            topLeft: State = .any,
+            top: State = .any,
+            topRight: State = .any,
+            leftEdge: Character? = nil,
+            topLeftCorner: Character? = nil,
+            topEdge: Character? = nil,
+            topRightCorner: Character? = nil
+        ) {
+            
+            _edgePatterns.append(
+                .init(
+                    stateMatch: state,
+                    leftStateMatch: left,
+                    topLeftStateMatch: topLeft,
+                    topStateMatch: top,
+                    topRightStateMatch: topRight,
+                    leftEdge: leftEdge,
+                    topLeftCorner: topLeftCorner,
+                    topEdge: topEdge,
+                    topRightCorner: topRightCorner
+                )
+            )
+        }
+
+        // Dark tiles
+        addRule(
+            state: .dark,
+            left: .dark,
+            leftEdge: Self._darkTileChar
+        )
+        addRule(
+            state: .dark,
+            top: .dark,
+            topEdge: Self._darkTileChar
+        )
+        addRule(
+            state: .dark,
+            left: .dark,
+            topLeft: .dark,
+            top: .dark,
+            topLeftCorner: Self._darkTileChar
+        )
+        // Edge between light -> dark tiles
+        addRule(
+            state: .dark,
+            left: .nonDark,
+            leftEdge: "▐"
+        )
+        addRule(
+            state: .dark,
+            left: .nonDark,
+            topLeft: .nonDark,
+            top: .dark,
+            topLeftCorner: "▐"
+        )
+        addRule(
+            state: .dark,
+            top: .nonDark,
+            topEdge: "▄"
+        )
+        addRule(
+            state: .dark,
+            left: .dark,
+            topLeft: .nonDark,
+            top: .nonDark,
+            topLeftCorner: "▄"
+        )
+        // Corners
+        addRule(
+            state: .dark,
+            left: .nonDark,
+            topLeft: .nonDark,
+            top: .nonDark,
+            topLeftCorner: "▗"
+        )
+        addRule(
+            state: .dark,
+            left: .lightOrUndecided,
+            topLeft: .dark,
+            top: .lightOrUndecided,
+            topLeftCorner: "▚"
+        )
+        addRule(
+            state: .dark,
+            left: .lightOrUndecided,
+            topLeft: .dark,
+            top: .dark,
+            topLeftCorner: "▜"
+        )
+        addRule(
+            state: .dark,
+            left: .dark,
+            topLeft: .lightOrUndecided,
+            top: .dark,
+            topLeftCorner: "▟"
+        )
+        addRule(
+            state: .dark,
+            left: .dark,
+            topLeft: .dark,
+            top: .lightOrUndecided,
+            topLeftCorner: "▙"
+        )
+
+        // Light tiles
+        // Edge between dark -> light tiles
+        addRule(
+            state: .nonDark,
+            left: .dark,
+            leftEdge: "▌"
+        )
+        addRule(
+            state: .nonDark,
+            top: .dark,
+            topEdge: "▀"
+        )
+        addRule(
+            state: .nonDark,
+            left: .dark,
+            topLeft: .dark,
+            top: .nonDark,
+            topLeftCorner: "▌"
+        )
+        addRule(
+            state: .nonDark,
+            left: .dark,
+            topLeft: .nonDark,
+            top: .nonDark,
+            topLeftCorner: "▖"
+        )
+        // Corners
+        addRule(
+            state: .nonDark,
+            left: .dark,
+            topLeft: .dark,
+            top: .dark,
+            topLeftCorner: "▛"
+        )
+        addRule(
+            state: .nonDark,
+            left: .nonDark,
+            topLeft: .dark,
+            top: .nonDark,
+            topLeftCorner: "▘"
+        )
+        addRule(
+            state: .nonDark,
+            left: .nonDark,
+            topLeft: .nonDark,
+            top: .dark,
+            topLeftCorner: "▝"
+        )
+        addRule(
+            state: .nonDark,
+            left: .nonDark,
+            topLeft: .dark,
+            top: .dark,
+            topLeftCorner: "▀"
+        )
+        addRule(
+            state: .nonDark,
+            left: .dark,
+            topLeft: .lightOrUndecided,
+            top: .dark,
+            topLeftCorner: "▞"
         )
     }
 
@@ -64,6 +246,36 @@ public class PatternGridPrinter: ConsolePrintBuffer {
         }
 
         joinBoxLines()
+
+        func stateAt(x: Int, y: Int) -> PatternTile.State? {
+            if x < 0 || y < 0 || x >= grid.columns || y >= grid.rows {
+                return nil
+            }
+
+            return grid[column: x, row: y].state
+        }
+
+        // Print joiners for dark cells across grid boundaries
+        
+        for y in 0...grid.rows {
+            for x in 0...grid.columns {
+                let boxX = startX + x * cellWidth
+                let boxY = startY + y * cellHeight
+
+                for pattern in self._edgePatterns {
+                    if pattern.matches(column: x, row: y, in: grid) {
+                        pattern.apply(
+                            buffer: self,
+                            cellX: boxX,
+                            cellY: boxY,
+                            cellWidth: cellWidth,
+                            cellHeight: cellHeight
+                        )
+                    }
+                }
+            }
+        }
+
         print()
     }
 
@@ -78,7 +290,7 @@ public class PatternGridPrinter: ConsolePrintBuffer {
         switch tile.state {
         case .undecided:
             fillRect(
-                char: "?",
+                char: Self._undecidedTileChar,
                 color: nil,
                 x: x,
                 y: y,
@@ -88,7 +300,7 @@ public class PatternGridPrinter: ConsolePrintBuffer {
         
         case .dark:
             fillRect(
-                char: "█",
+                char: Self._darkTileChar,
                 color: nil,
                 x: x,
                 y: y,
@@ -98,7 +310,7 @@ public class PatternGridPrinter: ConsolePrintBuffer {
 
         case .light:
             fillRect(
-                char: " ",
+                char: Self._lightTileChar,
                 color: nil,
                 x: x,
                 y: y,
@@ -109,6 +321,141 @@ public class PatternGridPrinter: ConsolePrintBuffer {
 
         // Draw surrounding tile lines
         putRect(x: x, y: y, w: width, h: height)
+    }
+
+    /// For performing matching square-like pattern matching to the grid pattern.
+    struct GridEdgePattern {
+        typealias State = PatternTile.State
+
+        var stateMatch: StateMatch
+
+        var leftStateMatch: StateMatch = .any
+        var topLeftStateMatch: StateMatch = .any
+        var topStateMatch: StateMatch = .any
+        var topRightStateMatch: StateMatch = .any
+
+        var leftEdge: Character?
+        var topLeftCorner: Character?
+        var topEdge: Character?
+        var topRightCorner: Character?
+
+        func matches(column: Int, row: Int, in grid: PatternGrid) -> Bool {
+            func stateOffset(dx: Int, dy: Int) -> PatternTile.State? {
+                let x = column + dx
+                let y = row + dy
+
+                if x < 0 || y < 0 || x >= grid.columns || y >= grid.rows {
+                    return nil
+                }
+
+                return grid[column: x, row: y].state
+            }
+
+            if !stateMatch.matches(stateOffset(dx: 0, dy: 0)) {
+                return false
+            }
+
+            if !leftStateMatch.matches(stateOffset(dx: -1, dy: 0)) {
+                return false
+            }
+            if !topLeftStateMatch.matches(stateOffset(dx: -1, dy: -1)) {
+                return false
+            }
+            if !topStateMatch.matches(stateOffset(dx: 0, dy: -1)) {
+                return false
+            }
+            if !topRightStateMatch.matches(stateOffset(dx: 1, dy: -1)) {
+                return false
+            }
+
+            return true
+        }
+
+        func apply(
+            buffer: ConsolePrintBuffer,
+            cellX: Int,
+            cellY: Int,
+            cellWidth: Int,
+            cellHeight: Int
+        ) {
+
+            if let leftEdge = leftEdge, cellHeight > 1 {
+                for y in (cellY + 1)..<(cellY + cellHeight) {
+                    buffer.putChar(leftEdge, x: cellX, y: y)
+                }
+            }
+            if let topLeftCorner = topLeftCorner {
+                buffer.putChar(topLeftCorner, x: cellX, y: cellY)
+            }
+            if let topEdge = topEdge, cellWidth > 1 {
+                for x in (cellX + 1)..<(cellX + cellWidth) {
+                    buffer.putChar(topEdge, x: x, y: cellY)
+                }
+            }
+            if let topRightCorner = topRightCorner {
+                buffer.putChar(topRightCorner, x: cellX + cellWidth, y: cellY)
+            }
+        }
+
+        enum StateMatch {
+            /// Matches dark tiles
+            case dark
+
+            /// Matches any tile that is not dark, including out-of-bounds
+            case nonDark
+            
+            /// Matches light tiles
+            case light
+
+            /// Matches undecided tiles
+            case undecided
+
+            /// Matches light and undecided tiles, but not dark tiles
+            case lightOrUndecided
+            
+            /// Matches any tile (except out-of-bounds)
+            case anyInBounds
+
+            /// Matches out-of-bounds tiles
+            case outOfBounds
+
+            /// Matches any tiles (in or out of bounds)
+            case any
+
+            /// Matches no tiles (in or out of bounds)
+            case none
+
+            func matches(_ state: PatternTile.State?) -> Bool {
+                switch self {
+                case .any:
+                    return true
+                
+                case .none:
+                    return false
+
+                case .anyInBounds:
+                    return state != nil
+
+                case .dark:
+                    return state == .dark
+                
+                case .nonDark:
+                    return state != .dark
+
+                case .light:
+                    return state == .light
+
+                case .undecided:
+                    return state == .undecided
+
+                case .lightOrUndecided:
+                    return state == .light || state == .undecided
+
+                case .outOfBounds:
+                    return state == nil
+                }
+            }
+        }
     }
 }
 
--- Tests/PatternSolverTests/PatternGridPrinterTests.swift
+++ Tests/PatternSolverTests/PatternGridPrinterTests.swift
@@ -19,7 +19,10 @@ class PatternGridPrinterTests: XCTestCase {
         gen.setTileState(column: 1, row: 0, .dark)
         gen.setTileState(column: 2, row: 0, .dark)
         gen.setTileState(column: 3, row: 0, .dark)
+        gen.setTileState(column: 1, row: 1, .dark)
         gen.setTileState(column: 2, row: 1, .light)
+        gen.setTileState(column: 1, row: 2, .dark)
+        gen.setTileState(column: 2, row: 2, .dark)
         let sut = PatternGridPrinter(bufferForGrid: gen.grid)
         sut.target = target
         sut.printGrid(grid: gen.grid)
@@ -29,17 +32,60 @@ class PatternGridPrinterTests: XCTestCase {
             """
                           1   2   2
                   1   2   1   2   2
-                ╭───┬───┬───┬───┬───╮
-              4 │???│███│███│███│???│
-                ├───┼───┼───┼───┼───┤
-            1 2 │???│???│   │???│???│
-                ├───┼───┼───┼───┼───┤
-              1 │???│???│???│???│???│
-                ├───┼───┼───┼───┼───┤
+                ╭───▗▄▄▄▄▄▄▄▄▄▄▄▖───╮
+              4 │???▐███████████▌???│
+                ├───▐███▛▀▀▀▀▀▀▀▘───┤
+            1 2 │???▐███▌   │???│???│
+                ├───▐███▙▄▄▄▖───┼───┤
+              1 │???▐███████▌???│???│
+                ├───▝▀▀▀▀▀▀▀▘───┼───┤
               2 │???│???│???│???│???│
                 ├───┼───┼───┼───┼───┤
               3 │???│???│???│???│???│
                 ╰───┴───┴───┴───┴───╯
+
+
+            """
+        )
+    }
+
+    func testPrintGrid_5x5_fullBox() throws {
+        // Game available at:
+        // https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/pattern.html#5x5:1/2/1.1/2.2/2.2/4/1.2/1/2/3
+        let gen = try PatternGridGenerator(gameId: "5x5:1/2/1.1/2.2/2.2/4/1.2/1/2/3")
+        gen.setTileState(column: 1, row: 0, .dark)
+        gen.setTileState(column: 2, row: 0, .dark)
+        gen.setTileState(column: 3, row: 0, .dark)
+        gen.setTileState(column: 0, row: 1, .light)
+        gen.setTileState(column: 1, row: 1, .dark)
+        gen.setTileState(column: 2, row: 1, .dark)
+        gen.setTileState(column: 3, row: 1, .dark)
+        gen.setTileState(column: 1, row: 2, .dark)
+        gen.setTileState(column: 2, row: 2, .dark)
+        gen.setTileState(column: 4, row: 2, .dark)
+        gen.setTileState(column: 3, row: 3, .dark)
+        gen.setTileState(column: 1, row: 4, .dark)
+        gen.setTileState(column: 2, row: 4, .dark)
+        let sut = PatternGridPrinter(bufferForGrid: gen.grid)
+        sut.target = target
+        sut.printGrid(grid: gen.grid)
+
+        XCTAssertEqual(
+            target.buffer,
+            """
+                          1   2   2
+                  1   2   1   2   2
+                ╭───▗▄▄▄▄▄▄▄▄▄▄▄▖───╮
+              4 │???▐███████████▌???│
+                ├───▐███████████▌───┤
+            1 2 │   ▐███████████▌???│
+                ├───▐███████▛▀▀▀▚▄▄▄▖
+              1 │???▐███████▌???▐███▌
+                ├───▝▀▀▀▀▀▀▀▚▄▄▄▞▀▀▀▘
+              2 │???│???│???▐███▌???│
+                ├───▗▄▄▄▄▄▄▄▞▀▀▀▘───┤
+              3 │???▐███████▌???│???│
+                ╰───▝▀▀▀▀▀▀▀▘───┴───╯
 
 
             """
