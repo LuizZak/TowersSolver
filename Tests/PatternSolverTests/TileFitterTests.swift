4f2448136ac5e409de5d1e2f3271e7aeab34c93a
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-11-07 14:38:25 -0300

    [PatternSolver] Fixing unit tests

--- Tests/PatternSolverTests/TileFitterTests.swift
+++ Tests/PatternSolverTests/TileFitterTests.swift
@@ -1083,7 +1083,7 @@ class TileFitterTests: XCTestCase {
 
     // MARK: - Private test factory methods
 
-    private func makeSut(hint: PatternGrid.RunsHint, tiles: [PatternTile]) -> TileFitter<[PatternTile]> {
+    private func makeSut(hint: PatternGrid.RunsHint, tiles: [PatternTile]) -> TileFitter {
         TileFitter(hint: hint, tiles: tiles)
     }
 
